e31922ed5e5f58c546a32ef1c301bb7f15ff4ed7
Kenan Velizade <Kenan.veli@yahoo.com>
2020-08-12 13:50:35 -0700

    some changings

---
+++ Admin/Controllers/SliderController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Admin.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Repository.Models;
+using Repository.Repositories.AdminPagesCrud;
+
+namespace Admin.Controllers
+{
+    [TypeFilter(typeof(Auth))]
+    public class SliderController : Controller
+    {
+        private Repository.Models.Admin _admin => RouteData.Values["Admin"] as Repository.Models.Admin;
+        private readonly ISliderItemRepository _SliderRepository;
+
+        public SliderController(ISliderItemRepository SliderRepository)
+        {
+            _SliderRepository = SliderRepository;
+        }
+        public IActionResult Index()
+        {
+            var Slider = _SliderRepository.GetAllSliderItem();
+            return View(Slider);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(SliderItem Slider)
+        {
+            if (ModelState.IsValid)
+            {
+                Slider.CreatedBy = _admin.Fullname;
+                _SliderRepository.CreateSliderItem(Slider);
+                return RedirectToAction("index");
+            }
+            return View(Slider);
+        }
+        public IActionResult Edit(int id)
+        {
+            SliderItem abs = _SliderRepository.GetSliderItemById(id);
+            if (abs == null) return NotFound();
+            return View(abs);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(SliderItem abs)
+        {
+            if (ModelState.IsValid)
+            {
+                abs.ModifiedBy = _admin.Fullname;
+                SliderItem SliderToUpdate = _SliderRepository.GetSliderItemById(abs.Id);
+                if (SliderToUpdate == null) return NotFound();
+                _SliderRepository.UpdateSliderItem(SliderToUpdate, abs);
+                return RedirectToAction("index");
+            }
+            return View(abs);
+        }
+        public IActionResult Delete(int id)
+        {
+            SliderItem abs = _SliderRepository.GetSliderItemById(id);
+            if (abs == null) return NotFound();
+            _SliderRepository.DeleteSliderItem(abs);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ Repository/Repositories/AdminPagesCrud/SliderItemRepository.cs
@@ -0,0 +1,71 @@
+﻿using Repository.Data;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository.Repositories.AdminPagesCrud
+{
+    public interface ISliderItemRepository
+    {
+        IEnumerable<SliderItem> GetSliderItem(int count);
+        IEnumerable<SliderItem> GetAllSliderItem();
+        void CreateSliderItem(SliderItem model);
+        SliderItem GetSliderItemById(int id);
+        void UpdateSliderItem(SliderItem SliderItemToUpdate, SliderItem model);
+        void DeleteSliderItem(SliderItem SliderItem);
+    }
+    public class SliderItemRepository : ISliderItemRepository
+    {
+        private readonly RetHouseDbContext _context;
+        public SliderItemRepository(RetHouseDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateSliderItem(SliderItem model)
+        {
+            model.CreatedAt = DateTime.Now;
+            _context.SliderItems.Add(model);
+            _context.SaveChanges();
+        }
+
+        public void DeleteSliderItem(SliderItem SliderItem)
+        {
+            _context.SliderItems.Remove(SliderItem);
+            _context.SaveChanges();
+        }
+
+        public SliderItem GetSliderItemById(int id)
+        {
+            return _context.SliderItems.Find(id);
+        }
+
+        public IEnumerable<SliderItem> GetSliderItem(int count)
+        {
+            return _context.SliderItems.Where(a => a.Status).Take(count).ToList();
+        }
+
+        public IEnumerable<SliderItem> GetAllSliderItem()
+        {
+            return _context.SliderItems.ToList();
+        }
+
+        public void UpdateSliderItem(SliderItem SliderItemToUpdate, SliderItem model)
+        {
+            SliderItemToUpdate.Status = model.Status;
+            SliderItemToUpdate.Title = model.Title;
+            SliderItemToUpdate.ActionText = "#";
+            SliderItemToUpdate.EndPoint = "#";
+            SliderItemToUpdate.Slogan = model.Slogan;
+            SliderItemToUpdate.Image = model.Image;
+            SliderItemToUpdate.OrderBy = model.OrderBy;
+            SliderItemToUpdate.ModifiedBy = model.ModifiedBy;
+            SliderItemToUpdate.ModifiedAt = DateTime.Now;
+            _context.SaveChanges();
+        }
+
+
+    }
+}
